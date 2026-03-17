4da62eca7e9f383e857f480d3ccf0171e181e317
ares2543 <batuhanfelek@hotmail.com>
2026-03-17 15:21:50 +0300

    mongodb atlas bağlandı

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/ordu-servis")
+mongoose.connect("mongodb+srv://ares:<B5252FLk>@cluster0.vw1q6eg.mongodb.net/?appName=Cluster0")
 .then(() => {
   console.log("MongoDB bağlandı");
 })
